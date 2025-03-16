ee4473610f3e48e92f756b979ca82cc4811a35af
KhaThach <khathach699@gmail.com>
2025-03-16 14:44:07 +0700

    add firebare

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:chat_app/Configs/PagePath.dart';
 import 'package:chat_app/Configs/Themes.dart';
 import 'package:chat_app/Pages/Welcome/WelcomePage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
